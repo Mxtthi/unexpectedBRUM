7a95546bbd6891e1bd7b37fbd397d1ce1e93ea7b
Mxtthi <matthias.keintzel@gmail.com>
2022-05-03 22:16:55 +0200

    game can detect multiple inputs at once now

--- controls.js
+++ controls.js
@@ -1,21 +1,35 @@
-document.onkeydown = function (e) {
-    e = e || window.event;
+var bKeys = [];
 
-    switch (e.key.toLowerCase()) {
-        case "w":
-            world.car.changeVelocity(1);
-            break;
-        case "s":
-            world.car.changeVelocity(-1);
-            break;
-        case "a":
-            world.car.changeRotation(-2);
-            break;
-        case "d":
-            world.car.changeRotation(2);
-            break;
-        default:
-            console.log("key not found");
-            break;
+$('body').keydown(function (e) {
+    if (bKeys.includes(e.which) === false) {
+        bKeys.push(e.which);
     }
-}
+
+    for (let i = 0; i < bKeys.length; i++) {
+        switch (bKeys[i]) {
+            case 87:
+                world.car.changeVelocity(1);
+                break;
+            case 83:
+                world.car.changeVelocity(-1);
+                break;
+            case 65:
+                world.car.changeRotation(-2);
+                break;
+            case 68:
+                world.car.changeRotation(2);
+                break;
+            default:
+                console.log("key not found");
+                break;
+        }
+    }
+});
+
+$('body').keyup(function (e) {
+    bKeys.pop(e.which);
+});
+setInterval(() => {
+    console.log(bKeys);
+}, 15);
+
